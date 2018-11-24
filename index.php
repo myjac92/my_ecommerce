b2e1fdd580877946e4ad9ab531a2ac74b7a320fa
josé Antônio <myjac92@gmail.com>
2018-11-23 23:05:25 -0200

    tpl site

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
 <?php
 
 require_once("vendor/autoload.php");
+use \Slim\Slim;
+use \Hcode\Page;
+use \Hcode\PageAdmin;
 
-$app = new \Slim\Slim();
+$app = new Slim();
 
 $app->config('debug', true);
-
-$app->get('/', function() {
-
-	$sql = new Hcode\DB\Sql();
-  $result = $sql->select("SELECT * FROM tb_users");
-  echo json_encode($result);
-
+//$templ ="/";
+$app->get("/", function() {
+  $page = new Page();
+  $page->setTpl("index");
 });
+
+$app->get("/", function() {
+  $page = new PageAdmin();
+  $page->setTpl("index");
+});
+
 
 $app->run();
 
